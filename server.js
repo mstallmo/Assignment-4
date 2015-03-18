6de8791c21b6bd261368c8a49b0086a45636eeef
Tejaswi Gowda <ec2-user@ip-10-176-161-26.us-west-1.compute.internal>
2015-03-18 09:15:09 +0000

    ac

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+var urlencode = require('urlencode');
+var json = require('json-middleware');
+
 var express = require("express"),
     app = express(),
     bodyParser = require('body-parser'),
     errorHandler = require('errorhandler'),
     methodOverride = require('method-override'),
     hostname = process.env.HOSTNAME || 'localhost',
-    port = parseInt(process.env.PORT, 10) || 4567;
+    port = 8080;
 
 app.get("/", function (req, res) {
   res.redirect("/index.html");
@@ -72,19 +75,51 @@ app.get("/listtodos", function (req, res) {
 	});
 });
 
-
-
-
 app.use(methodOverride());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(express.static(__dirname + '/client'));
 app.use(errorHandler({
   dumpExceptions: true,
   showStack: true
 }));
+
+
+var fs = require('fs');
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./credentials.json');
+var s3 = new AWS.S3()//.client;
+console.log(s3)
+
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+app.use('/uploadFile', multipartMiddleware);
+
+app.post('/uploadFile', function(req, res){
+    console.log(req.body);
+     var intname = req.body.fileInput;
+     var filename = req.files.input.name;
+     var fileType =  req.files.input.type;
+     var tmpPath = req.files.input.path;
+     var s3Path = '/' + intname;
+                            
+     fs.readFile(tmpPath, function (err, data) {
+         var params = {
+             Bucket:'ameweb',
+             ACL:'public-read',
+             Key:intname,
+             Body: data
+         };
+         s3.putObject(params, function(err, data) {
+             console.log(err);
+             res.end("success");
+         });
+     });
+});
+
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
 app.listen(port, hostname);
